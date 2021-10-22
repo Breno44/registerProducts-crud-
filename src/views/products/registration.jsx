7c127dd9c1b0e080f35b584c9b00efaaf9c952af
Breno <bhfs.2908@gmail.com>
2021-10-22 16:39:15 -0300

    feat:creating the product card

---
+++ src/views/products/registration.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class RegistrationProduct extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header">Cadastro de produto</div>
+        <div className="card-body"></div>
+      </div>
+    );
+  }
+}
