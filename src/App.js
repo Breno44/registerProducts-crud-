791db53fd94d12cd4b12dcf559a9dfc92a87505e
Breno <bhfs.2908@gmail.com>
2021-10-22 16:07:42 -0300

    feat:add bootswatch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="alert alert-dismissible alert-warning">
+      <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
+      <h4 className="alert-heading">Warning!</h4>
+      <p className="mb-0">
+        Hello
+        <a href="/" className="alert-link">
+          World
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
