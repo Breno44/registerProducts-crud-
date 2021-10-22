9d96d552a5eb5f3f36f1735aaa0677c95070a504
Breno <bhfs.2908@gmail.com>
2021-10-22 16:17:30 -0300

    feat:implementing navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
+import Navbar from "./components/navbar";
+
 export default function App() {
   return (
-    <div className="alert alert-dismissible alert-warning">
-      <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
-      <h4 className="alert-heading">Warning!</h4>
-      <p className="mb-0">
-        Hello
-        <a href="/" className="alert-link">
-          World
-        </a>
-        .
-      </p>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
